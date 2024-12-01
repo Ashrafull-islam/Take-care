905f5e88acf473028c413edab2b985e91e39328c
Ashraful islam <mmmdashrafull211@gmail.com>
2024-12-01 19:19:41 +0600

    Fix ESLint errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint:{
+    ignoreDuringBuilds:true
+  },
   images: {
     domains: ['i.ibb.co'], // Add the domain here
   },
